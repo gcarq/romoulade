f9f9b304846e6dbea8acb80af868dc58f1a7b50d
gcarq <egger.m@protonmail.com>
2025-03-26 19:04:33 +0100

    Remove panic hook
    Signed-off-by: gcarq <egger.m@protonmail.com>

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,12 @@
 extern crate bitflags;
 extern crate clap;
 use crate::gui::Romoulade;
-use backtrace::Backtrace;
 use clap::Parser;
 use eframe::{HardwareAcceleration, egui};
 use std::error::Error;
-use std::panic;
-use std::panic::PanicHookInfo;
 
 mod gb;
 mod gui;
-mod utils;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -24,10 +20,6 @@ struct Args {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    panic::set_hook(Box::new(|info| {
-        panic_hook(info);
-    }));
-
     let args = Args::parse();
     if args.debug {
         // TODO: Implement debugger
@@ -47,28 +39,4 @@ fn main() -> Result<(), Box<dyn Error>> {
     eframe::run_native("Romoulade", options, Box::new(|_| Ok(Box::new(app))))
         .expect("Unable to run egui app");
     Ok(())
-}
-
-fn panic_hook(info: &PanicHookInfo<'_>) {
-    if cfg!(debug_assertions) {
-        let location = info.location().unwrap();
-
-        let msg = match info.payload().downcast_ref::<&'static str>() {
-            Some(s) => *s,
-            None => match info.payload().downcast_ref::<String>() {
-                Some(s) => &s[..],
-                None => "Box<Any>",
-            },
-        };
-
-        let stacktrace: String = format!("{:?}", Backtrace::new()).replace('\n', "\n\r");
-
-        println!(
-            "{}thread '<unnamed>' panicked at '{}', {}\n\r{}",
-            termion::screen::ToMainScreen,
-            msg,
-            location,
-            stacktrace
-        );
-    }
 }
