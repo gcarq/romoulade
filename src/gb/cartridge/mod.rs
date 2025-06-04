f3e37593e7263551e99ddf4e0a6a23f09ef6e0d4
gcarq <egger.m@protonmail.com>
2025-06-04 20:05:35 +0200

    cartridge: Move tests into inline module
    Signed-off-by: gcarq <egger.m@protonmail.com>

--- src/gb/cartridge/mod.rs
+++ src/gb/cartridge/mod.rs
@@ -11,8 +11,6 @@ mod mbc1;
 mod mbc3;
 mod mbc5;
 mod nombc;
-#[cfg(test)]
-mod tests;
 
 /// This area of memory contains the cartridge title
 const CARTRIDGE_TITLE_BEGIN: u16 = 0x0134;
@@ -348,4 +346,59 @@ fn calculate_global_checksum(buf: &[u8]) -> u16 {
 const fn bank_mask(rom_banks: u16) -> u32 {
     let mask = u16::BITS - rom_banks.leading_zeros();
     (1 << mask) - 1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_calculate_global_checksum() {
+        let buf = (0..CARTRIDGE_GLOBAL_CHECKSUM2)
+            .map(|i| i as u8)
+            .collect::<Vec<u8>>();
+        let checksum = calculate_global_checksum(&buf);
+        assert_eq!(checksum, 0x8B3B);
+    }
+
+    #[test]
+    fn test_verify_checksum_ok() {
+        let mut buf = (0..=CARTRIDGE_GLOBAL_CHECKSUM2)
+            .map(|i| i as u8)
+            .collect::<Vec<u8>>();
+        buf[CARTRIDGE_GLOBAL_CHECKSUM1 as usize] = 0x8B;
+        buf[CARTRIDGE_GLOBAL_CHECKSUM2 as usize] = 0x3B;
+        assert!(verify_checksum(&buf, 0x8B3B).is_ok());
+    }
+
+    #[test]
+    fn test_verify_checksum_buffer_invalid_checksum() {
+        let mut buf = (0..=CARTRIDGE_GLOBAL_CHECKSUM2)
+            .map(|i| i as u8)
+            .collect::<Vec<u8>>();
+        buf[CARTRIDGE_GLOBAL_CHECKSUM1 as usize] = 0x00;
+        buf[CARTRIDGE_GLOBAL_CHECKSUM2 as usize] = 0x00;
+        assert!(verify_checksum(&buf, 0x0000).is_err());
+    }
+
+    #[test]
+    fn test_cartridge_config() {
+        let config =
+            CartridgeConfig::new(ControllerType::MBC1 { battery: true }, 0x02, 0x03).unwrap();
+        assert_eq!(config.controller, ControllerType::MBC1 { battery: true });
+        assert_eq!(config.rom_banks, 8);
+        assert_eq!(config.ram_size(), 32768);
+        assert_eq!(config.ram_banks, 4);
+    }
+
+    #[test]
+    fn test_bank_mask() {
+        assert_eq!(bank_mask(2), 0b11);
+        assert_eq!(bank_mask(4), 0b111);
+        assert_eq!(bank_mask(8), 0b1111);
+        assert_eq!(bank_mask(16), 0b11111);
+        assert_eq!(bank_mask(32), 0b111111);
+        assert_eq!(bank_mask(64), 0b1111111);
+        assert_eq!(bank_mask(128), 0b11111111);
+    }
 }
